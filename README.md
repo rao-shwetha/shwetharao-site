b8e2e9bd7645c17db0b9685a380654f62fcac3bb
shwe-13 <rao.b.shwetha@gmail.com>
2021-02-24 18:52:16 +0400

    blog project section updated

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Shwetha Rao Portfolio
 This website is for displaying my personal portfolio.
-For now, I know: 
-1. Javascript
-1. NodeJS
-1. Repl.it
-1. CHALK
--- additionalcomponents.js
+++ additionalcomponents.js
@@ -7,7 +7,6 @@ var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     /**alert("Sticky is " + sticky);**/
@@ -20,3 +19,4 @@ function myFunction() {
     document.getElementById("navbar").style.padding = "5px 10px";
   }
 }
+
